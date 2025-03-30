04e9f9f2ec92e0fd142f4e256989fb9325259819
Daniel Otero <jdanielotero01@gmail.com>
2025-03-29 23:50:09 -0500

    Basic Files

---
+++ Views/usuariosView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <td>Usuarios</td>
+                <td>Constrasenia</td>
+                <td>Nombre</td>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+</body>
+</html>
